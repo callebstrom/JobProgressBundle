51d5839e7adc86b729e3bfb49400c5f5b529a1e4
callebstrom <calle.bergstrom@live.se>
2015-07-19 00:44:07 +0200

    Generated JobHandleController

---
+++ Controller/JobHandleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace callebstrom\JPBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+#########################################################################
+# Controller class used to create jobs and update progress via Doctrine #
+#########################################################################
+
+class JobHandleController extends Controller
+{
+}
