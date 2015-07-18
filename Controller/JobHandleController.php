62b9780d942b19e2bc0f5a247f2691aa8d8b2bbf
callebstrom <calle.bergstrom@live.se>
2015-07-19 01:00:52 +0200

    Added initJob functionality

--- Controller/JobHandleController.php
+++ Controller/JobHandleController.php
@@ -7,12 +7,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Doctrine\ORM\Mapping as ORM;
 
+use callebstrom\JPBundle\Entity\Job;
+
 #########################################################################
 # Controller class used to create jobs and update progress via Doctrine #
 #########################################################################
 
 class JobHandleController extends Controller
 {
+
+	// Var that stores the job id
+	private $jobId = null;
+
 	public function addProgressAction() {
 
 	}
@@ -21,7 +27,27 @@ class JobHandleController extends Controller
 
 	}
 
-	public function initJobAction() {
+	// Func that inits new job with custom type 
+	public function initJobAction($type) {
 
+		// Init new job
+		$job = new Job();
+
+		// Set progress to 0
+		$job->setProgress = 0;
+		
+		// Set Job type based on arg
+		$job->setType($type);
+		
+		// Set startdate to current datetime using MySQL DATETIME format
+		$job->setStartDate(date("Y-m-d H:i:s"));
+
+		// Store the Job entity in the DB
+		$em = $this->getDoctrine()->getEntityManager();
+				$em->persist($job);
+				$em->flush();
+
+		// Set the job ID if Doctrine successfully stored the object
+		$this->jobId = $job->getId();
 	}
 }
