0e0418fbfbc222b5eaf7ea86dbc43290e479bdb4
callebstrom <calle.bergstrom@live.se>
2015-07-19 00:49:15 +0200

    Added initJobAction to JobHandle and included ORM

--- Controller/JobHandleController.php
+++ Controller/JobHandleController.php
@@ -5,6 +5,7 @@ namespace callebstrom\JPBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Doctrine\ORM\Mapping as ORM;
 
 #########################################################################
 # Controller class used to create jobs and update progress via Doctrine #
@@ -12,4 +13,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class JobHandleController extends Controller
 {
+	public function addProgressAction() {
+
+	}
+
+	public function finishJobAction() {
+
+	}
+
+	public function initJobAction() {
+
+	}
 }
